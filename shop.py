8a34be19fb70a5b316058d68c8edd78adf61c3db
Evelynezz <gevelynezzs@gmail.com>
2025-04-27 08:26:15 +0300

    Добавлен shop.py
    Добавлен файл, содержащий словарь с данными о животных и кормах

---
+++ shop.py
@@ -0,0 +1,57 @@
+# Данные о животных
+animals = {
+    'кот': {
+        'price': 100,
+        'visitor_multiplier': 1.0,
+        'feed_cost': 10,
+        'emoji': '🐱',
+        'initial_count': 1,
+        'hungry': False
+
+    },
+    'собака': {
+        'price': 100,
+        'visitor_multiplier': 1.2,
+        'feed_cost': 1.0,
+        'emoji': '🐶',
+        'hungry': False
+
+    },
+    'кролик': {
+        'price': 200,
+        'visitor_multiplier': 1.3,
+        'feed_cost': 0.8,
+        'emoji': '🐰',
+        'hungry': False
+
+    },
+    'крокодил': {
+        'price': 500,
+        'visitor_multiplier': 1.5,
+        'feed_cost': 3.0,
+        'emoji': '🐊',
+        'hungry': False
+    },
+    'слон': {
+        'price': 1000,
+        'visitor_multiplier': 2.0,
+        'feed_cost': 5.0,
+        'emoji': '🐘',
+        'hungry': False
+    },
+    'жираф': {
+        'price': 800,
+        'visitor_multiplier': 1.8,
+        'feed_cost': 4.0,
+        'emoji': '🦒',
+        'hungry': False
+    }
+}
+
+
+food = {
+    'корм': {
+        'price': 5,
+        'quantity': 1,
+        'emoji': '🌾',
+        'description': 'единица корма'}}
