5ef154e726e8d96ea10114c37ff49ec6aa5a2d42
Evelynezz <gevelynezzs@gmail.com>
2025-04-30 21:02:46 +0300

    Доработка животных и фразы инспектора
    Доработаны животные (снижены множители посещений, теперь доход игрока ниже и развитие зоопарка происходит медленнее).
    Добавлены случайные фразы инспектора

--- shop.py
+++ shop.py
@@ -1,51 +1,127 @@
-# Данные о животных
 animals = {
     'кот': {
-        'price': 100,
-        'visitor_multiplier': 1.0,
-        'feed_cost': 10,
-        'emoji': '🐱',
-        'initial_count': 1,
-        'hungry': False
-
-    },
-    'собака': {
-        'price': 100,
-        'visitor_multiplier': 1.2,
-        'feed_cost': 1.0,
-        'emoji': '🐶',
-        'hungry': False
-
+        'price': 200,
+        'visitor_multiplier': 0.1,
+        'feed_cost': 5,
+        'emoji': '🐈',
+        'initial_count': 1
     },
     'кролик': {
-        'price': 200,
-        'visitor_multiplier': 1.3,
-        'feed_cost': 0.8,
-        'emoji': '🐰',
-        'hungry': False
-
+        'price': 400,
+        'visitor_multiplier': 0.2,
+        'feed_cost': 10,
+        'emoji': '🐇'
+    },
+    'ёж': {
+        'price': 600,
+        'visitor_multiplier': 0.25,
+        'feed_cost': 15,
+        'emoji': '🦔'
+    },
+    'попугай': {
+        'price': 800,
+        'visitor_multiplier': 0.4,
+        'feed_cost': 20,
+        'emoji': '🦜'
+    },
+    'черепаха': {
+        'price': 1000,
+        'visitor_multiplier': 0.5,
+        'feed_cost': 25,
+        'emoji': '🐢'
+    },
+    'змея': {
+        'price': 1300,
+        'visitor_multiplier': 0.65,
+        'feed_cost': 30,
+        'emoji': '🐍'
+    },
+    'лиса': {
+        'price': 1750,
+        'visitor_multiplier': 0.85,
+        'feed_cost': 35,
+        'emoji': '🦊'
+    },
+    'обезьяна': {
+        'price': 2000,
+        'visitor_multiplier': 1.0,
+        'feed_cost': 40,
+        'emoji': '🐒'
+    },
+    'коала': {
+        'price': 2300,
+        'visitor_multiplier': 1.15,
+        'feed_cost': 45,
+        'emoji': '🐨'
+    },
+    'ленивец': {
+        'price': 2750,
+        'visitor_multiplier': 1.4,
+        'feed_cost': 50,
+        'emoji': '🦥'
+    },
+    'лама': {
+        'price': 3000,
+        'visitor_multiplier': 1.5,
+        'feed_cost': 55,
+        'emoji': '🦙'
+    },
+    'бурый медведь': {
+        'price': 3500,
+        'visitor_multiplier': 1.75,
+        'feed_cost': 65,
+        'emoji': '🐻'
+    },
+    'белый медведь': {
+        'price': 4000,
+        'visitor_multiplier': 2.0,
+        'feed_cost': 70,
+        'emoji': '🐻‍❄️'
+    },
+    'панда': {
+        'price': 4500,
+        'visitor_multiplier': 2.25,
+        'feed_cost': 75,
+        'emoji': '🐼️'
+    },
+    'тигр': {
+        'price': 5000,
+        'visitor_multiplier': 2.5,
+        'feed_cost': 80,
+        'emoji': '🐅'
+    },
+    'бегемот': {
+        'price': 5500,
+        'visitor_multiplier': 2.75,
+        'feed_cost': 85,
+        'emoji': '🦛'
+    },
+    'носорог': {
+        'price': 6000,
+        'visitor_multiplier': 3.0,
+        'feed_cost': 90,
+        'emoji': '🦏'
     },
     'крокодил': {
-        'price': 500,
-        'visitor_multiplier': 1.5,
-        'feed_cost': 3.0,
-        'emoji': '🐊',
-        'hungry': False
-    },
-    'слон': {
-        'price': 1000,
-        'visitor_multiplier': 2.0,
-        'feed_cost': 5.0,
-        'emoji': '🐘',
-        'hungry': False
+        'price': 7000,
+        'visitor_multiplier': 3.5,
+        'feed_cost': 95,
+        'emoji': '🐊'
     },
     'жираф': {
-        'price': 800,
-        'visitor_multiplier': 1.8,
-        'feed_cost': 4.0,
-        'emoji': '🦒',
-        'hungry': False
+        'price': 8000,
+        'visitor_multiplier': 4.0,
+        'feed_cost': 100,
+        'emoji': '🦒'
+    },
+    'слон': {
+        'price': 10000,
+        'visitor_multiplier': 5.0,
+        'feed_cost': 120,
+        'emoji': '🐘'
     }
+
+
 }
 
 
@@ -55,3 +131,12 @@ food = {
         'quantity': 1,
         'emoji': '🌾',
         'description': 'единица корма'}}
+
+
+# рандомные фразы инспектора
+inspector_phrases = [
+    '👮 Инспектор зоопарков на связи! Если ты не начнешь кормить своих животных, они могут начать писать мемуары о своей голодной жизни!',
+    '👮 Инспектор зоопарков: "Если вы не покормите своих животных, то они начнут требовать зарплату за свою работу по развлечению посетителей!"',
+    '📞 Ало! Это Инспектор зоопарков. Мы получили многочисленные жалобы от ваших животных! Вам следует немедленно покормить их.',
+    '😿 Это служба защиты животных. Вчера в наш главный офис пришёл ваш кот, который был вынужден перейти на веганскую диету. Что происходит в вашем зоопарке?!',
+    '😡 Животные из соседнего зоопарка жалуются на громкое осуждающее рычание по ночам и не могут заснуть! Вам нужно что-то немедленно предпринять.']
